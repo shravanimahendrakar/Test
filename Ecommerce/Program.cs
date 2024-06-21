e3a70a09342c3012355bf3c055c9c47ba2299f7b
shravanimahendrakar <mahendrakarshravani@gmail.com>
2024-06-20 20:21:57 -0400

    Add project files.

---
+++ Ecommerce/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ecommerce;
+
+Console.WriteLine("Hello, World!");
+
+Product p = new Product(112, "Mobile", 758.55, 2);
+p.IncreaseStock(3);
+Console.WriteLine($"Increase stock " + p.Stock);
+p.DecreaseStock(1);
+Console.WriteLine($"Decrease stock " + p.Stock);
